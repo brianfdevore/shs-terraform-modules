8382848fd4e17908f556d09fb60a080d14f67403
brianfdevore <brianfdevore@gmail.com>
2020-09-17 16:06:49 -0400

    update outputs.tf

--- ari-shop/pm/win/sample-dotnet-app/outputs.tf
+++ ari-shop/pm/win/sample-dotnet-app/outputs.tf
@@ -1,4 +1,4 @@
 output "instance_source_image" {
-  value = google_compute_instance.vm_instance.boot_disk.image
+  value = "sample" #google_compute_instance.vm_instance.boot_disk.image
   description = "The source image that was used to create this instance."
 }
