f02096c31e1090c297243dc5dbdedbc78c62fe27
brianfdevore <brianfdevore@gmail.com>
2020-09-18 00:54:12 -0400

    change metadata

--- ari-shop/pm/win/sample-dotnet-app/main.tf
+++ ari-shop/pm/win/sample-dotnet-app/main.tf
@@ -34,7 +34,7 @@ resource "google_compute_instance" "vm_instance" {
   metadata = {
     enable-oslogin  = "True"
     app-environment = var.env
-    owner = "Brian DeVore"
+    asset-owner = "Brian DeVore"
     #windows-startup-script-ps1 = TODO - https://cloud.google.com/compute/docs/startupscript
   }
 
